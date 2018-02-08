36b1a8e65a77e524fdc6733e795d7c158b17ae21
Gleisson S Silva <toddys05@yahoo.com.br>
2018-02-08 14:04:45 +0000

    add mongolab

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var commentRoutes     = require("./routes/comments"),
     campgroundRoutes  = require("./routes/campgrounds"),
     indexRoutes        = require("./routes/index")
 
-mongoose.connect('mongodb://localhost/yelp_camp_v10');
+//mongoose.connect('mongodb://localhost/yelp_camp_v10');
+mongoose.connect('mongodb://gleisson:gleisson@ds125318.mlab.com:25318/toddyelpcamp');
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
