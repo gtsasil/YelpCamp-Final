6dc6ae209eaf17e57e4491b2b67442ceabc5ff7d
Gleisson S Silva <toddys05@yahoo.com.br>
2018-02-09 20:32:50 +0000

    update db url

--- app.js
+++ app.js
@@ -16,8 +16,10 @@ var commentRoutes     = require("./routes/comments"),
     campgroundRoutes  = require("./routes/campgrounds"),
     indexRoutes        = require("./routes/index")
 
-//mongoose.connect('mongodb://localhost/yelp_camp_v10');
-mongoose.connect('mongodb://gleisson:gleisson@ds125318.mlab.com:25318/toddyelpcamp');
+console.log(process.env.DATABASEURL);
+
+mongoose.connect(process.env.DATABASEURL);
+//mongoose.connect('mongodb://gleisson:gleisson@ds125318.mlab.com:25318/toddyelpcamp');
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
